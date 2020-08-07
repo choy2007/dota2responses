9d42a8a565b281ca2ca7b3c588ac633a6058a6e9
choy2007 <choymanzanoalberto@gmail.com>
2020-08-07 10:45:10 +0800

    Fix wss url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Specify AnyCable WebSocket server URL to use by JS client
   config.after_initialize do
-    config.action_cable.url = "ws://dota2responses.herokuapp.com/cable"
+    config.action_cable.url = "wss://dota2responses.herokuapp.com/cable"
   end
   # Settings specified here will take precedence over those in config/application.rb.
 
